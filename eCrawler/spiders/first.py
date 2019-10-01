57a594afdcc5eeb2ebbb4732837814861ddd4e1b
Geovani da Silva Júnior <geovanij3@gmail.com>
2019-09-30 21:56:27 -0300

    Modified first.py to get team names from hltv and save in a csv file

--- eCrawler/spiders/first.py
+++ eCrawler/spiders/first.py
@@ -1,27 +1,35 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 import scrapy
 from scrapy.selector import Selector
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
+import csv
+import time
+
 
 class FirstSpider(CrawlSpider):
     name = 'first'
-    
-    #allowed_domains = ['https://liquipedia.net']
+
+    #allowed_domains = ['https://liquipedia.net/']
 
     #urls para startar o crawler
-    start_urls = ('https://liquipedia.net/counterstrike/ESL/One/2019/New_York',)
+    start_urls = ['https://www.hltv.org/team/9215/mibr']
     rules = (Rule(LinkExtractor(), callback = 'parse_page', follow=True),)
 
     def parse_page(self, response):
 
-        groupsolt = response.xpath('//*[@class="grouptableslot"]').extract()
-        teamName = response.xpath('//*[@class="team-template-text"]').extract()
-        teamLogo = response.xpath('//*[@class="team-template-image"]').extract()
+        #groupsolt = response.xpath('//[@class="grouptableslot"]').extract()
+        teamNames = response.xpath('//div[contains(@class, "team-name")]/text()').extract()
+        teamNames = set(map(lambda x: x.strip(), teamNames)) - {''}
 
-        yield {'teamName':teamName, 'teamlogo': teamLogo}
+        with open('teams.csv', 'w') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            spamwriter.writerow(['Team Name'])
+            for teamName in teamNames:
+                spamwriter.writerow([teamName])
 
-            
+        print('Ctrl + C')
+        time.sleep(5000)
 
-    
+        yield {'teamName':teamNames}
