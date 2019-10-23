ab96811a2c40c2b1a1c218798530a51c32e4d360
Daniel Baulé <dsbaule@gmail.com>
2019-10-23 11:24:40 -0300

    better plotting of age and countries

--- src/ageplot.py
+++ src/ageplot.py
@@ -1,10 +1,35 @@
 import json
 import matplotlib.pyplot as plt
-import pandas as pd
+import pandas
 
-with open('../eCrawler/players.json') as json_file:
-	players = json.load(json_file)
-	ages = [int(player['age'].split()[0]) for player in players]
-	plt.hist(ages, bins=range(min(ages) - 1,max(ages) + 2))
-	plt.title("Histogram with 'auto' bins")
-	plt.show()
+# Load Json Data
+players = pandas.read_json ('../eCrawler/players.json')
+
+# Fix ages
+players['age'] = [(age.split()[0]) for age in players['age']]
+
+# Generate frequencies
+ages = players['age'].value_counts(sort=False).sort_index()
+countries = players['country'].value_counts(sort=False).sort_index()
+
+# Checking for duplicates
+nicks = players['nick'].value_counts()
+names = players['name'].value_counts()
+print(nicks)
+print(names)
+
+
+# Init plot
+plt.close('all')
+plt.figure()
+
+# Plot ages
+plt.subplot(2, 1, 1)
+ages.plot(kind='bar')
+
+# Plot countries
+plt.subplot(2, 1, 2)
+countries.plot(kind='bar')
+
+# Show plot
+plt.show()
