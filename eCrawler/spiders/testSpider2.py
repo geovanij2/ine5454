fce6a1a96445bdf8f8c99a65e737033f2b4924b2
Geovani da Silva Júnior <geovanij3@gmail.com>
2019-11-19 22:46:17 -0300

    Termina estratégia de pegar dados das divs vizinhas de uma div que contenha VS

--- eCrawler/spiders/testSpider2.py
+++ eCrawler/spiders/testSpider2.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from bs4 import BeautifulSoup
 # http://www.hltv.org/matches/2337808/havu-vs-cr4zy-esea-mdl-season-32-europe//
+
+def contains_vs_icon(div):
+    for string in div.strings:
+        if string in {'vs'}:
+            return True
+    return False
 
 class Testspider2Spider(scrapy.Spider):
     name = 'testSpider2'
@@ -17,11 +24,28 @@ class Testspider2Spider(scrapy.Spider):
 
         i = 0
         for div in divs:
-            if div is not None:
-                file1.write( \
-                    '\n-------------------------------------------------- \n' +\
-                    '    DIV # ' + str(i) + \
-                    '\n-------------------------------------------------- \n' \
-                    + div)
+            soup = BeautifulSoup(div, "html.parser")
+            children = soup.div.contents
+            print('-------------------------------------------------\n')
+            while '\n' in children:
+                children.remove('\n')
+            if len(children) == 3 and contains_vs_icon(children[1]):
+                file1.write('+++++++++')
+                for string in children[0].strings:
+                    file1.write(string)
+                file1.write('+++++++++')
+                # for string in children[1].strings:
+                #     file1.write(string)
+                # file1.write('+++++++++')
+                for string in children[2].strings:
+                    file1.write(string)
+                file1.write('+++++++++')
+            print('-------------------------------------------------\n')
+            # if div is not None:
+            #     file1.write( \
+            #         '\n-------------------------------------------------- \n' +\
+            #         '    DIV # ' + str(i) + \
+            #         '\n-------------------------------------------------- \n' \
+            #         + div)
             i += 1
         file1.close()
